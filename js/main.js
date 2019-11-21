c07f86acd104ff5f6f66e054abf2ad03d158d6cf
Chicolajtrue <38371144+Chicolajtrue@users.noreply.github.com>
2019-11-21 19:48:01 +0300

    очередные изменения
    больше адаптива плюс скрывающяяся панель навигации плюс доработка формы связи

--- js/main.js
+++ js/main.js
@@ -5,3 +5,13 @@ $(function(){
                 return false;
         });
 });
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
